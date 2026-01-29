1f0b1764839a8371057c6b400e4a7a29c4e9e5fb
damzG <C00313475@setu.ie>
2026-01-29 14:56:01 +0000

    Added new files

--- AddOffice.php
+++ AddOffice.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Office Report</title>
+    <title>Add New Office</title>                                                   
 </head>
 <body>
     <h2>Add New Office Property</h2>
